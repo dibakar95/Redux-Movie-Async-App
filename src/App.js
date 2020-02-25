fdb95ca76d370e4594f4ef11adc1c300b0887809
dibakar <dibakar.redbulls@gmail.com>
2020-02-25 12:19:30 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import updateMovies from "./store/actions/updateMovies";
+import fetchUsers from "./store/actions/fetchUsers";
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>React MovieList App</h3>
+      <br />
+      <span style={{ color: "green" }}>
+        Your Current Movie is: {props.movies.name}
+      </span>
+      <br />
+      <button onClick={props.updateMovies}>Select New Movie</button>
+      <br />
+      {props.users.length === 0 ? (
+        <p style={{ color: "Red" }}>There are no Users</p>
+      ) : (
+        props.users.map((user) => (
+          <p key={user.id} >
+            {user.first_name} - {user.email}
+            <div><img src={user.avatar} alt= {user.first_name} height="80" width="80"/></div>
+          </p>
+        ))
+      )}
+      <br />
+      <button onClick = {props.fetchUsers}>Fetch Users</button>
     </div>
   );
 }
-
-export default App;
+const MapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+    users: state.users
+  };
+};
+const MapDispatchToProps = (dispatch) => {
+  return {
+    updateMovies: () => dispatch(updateMovies),
+    fetchUsers: () => dispatch(fetchUsers)
+  };
+};
+export default connect(MapStateToProps, MapDispatchToProps)(App);
